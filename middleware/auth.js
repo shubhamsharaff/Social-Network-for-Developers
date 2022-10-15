20d02a726961b2cace96dbacc31b6b4bab6ec425
Shubham Sharaff <sharaff0shubham@gmail.com>
2022-10-15 23:37:32 +0530

    Updations done on user authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,6 +6,18 @@ module.exports = function (req, res, next) {
     // Get token from the header
     const token = req.header('x-auth-token');
 
-    // 
+    // check if not token 
+    if(!token){
+        return res.status(401).json({msg : 'No token , authorization denied'})
+    }
+    // verify token 
+    try{
+        const decoded = jwt.verify(token , config.get('jwtSecret'));
 
-}
+        req.user = decoded.user;
+        next();
+    }
+    catch(err){
+        res.status(401).json({msg : 'Token is not valid '})
+    }
+};
