6291a26c62a1e8f0f50feeb02718d9b23a92cfbf
Shubham Sharaff <sharaff0shubham@gmail.com>
2022-10-17 21:58:12 +0530

    Changes on profile.js api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,27 +1,75 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth')
-const Profile= require('../../models/Profile')
-const User= require('../../models/User')
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route   GET api/users/me
-// @desc    Get current users profile by passing token 
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    Get current users profile by passing token
 // @access  Private
-router.get('/me',auth, async (req, res) => {
-    try{
-        const profile = await Profile.findOne({user : req.user.id}).populate(
-            'user',
-            ['name','avatar']
-        );
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
 
-        if(!profile){
-            return res.status(400).json({msg : 'There is no profile for this user'})
-        }
-        res.json(profile);
-    }catch(err){
-        console.log(err.message)
-        res.send(500).send('Server Error')
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
     }
-})
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.send(500).send('Server Error');
+  }
+});
+
+// @route   Post api/profile/
+// @desc    Create or update Profile
+// @access  Public
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    // if there are errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // destructure the request
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = res.body;
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(company) profileFields.company=company
+    if(website) profileFields.website=website
+    if(location) profileFields.location=location
+    if(bio) profileFields.bio=bio
+    if(status) profileFields.status=status
+    if(githubusername) profileFields.githubusername=githubusername
+    // start with this line
+  }
+);
 
 module.exports = router;
