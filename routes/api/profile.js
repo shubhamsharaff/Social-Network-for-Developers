450312140f1e1480363034c406cfca0894832309
Shubham Sharaff <shubhamsharaff@blr1.cdac.in>
2022-10-18 19:10:20 +0530

    Profile api added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -58,17 +58,53 @@ router.post(
       twitter,
       instagram,
       linkedin,
-    } = res.body;
+    } = req.body;
 
     const profileFields = {};
     profileFields.user = req.user.id;
-    if(company) profileFields.company=company
-    if(website) profileFields.website=website
-    if(location) profileFields.location=location
-    if(bio) profileFields.bio=bio
-    if(status) profileFields.status=status
-    if(githubusername) profileFields.githubusername=githubusername
-    // start with this line
+    if (company) profileFields.company = company
+    if (website) profileFields.website = website
+    if (location) profileFields.location = location
+    if (bio) profileFields.bio = bio
+    if (status) profileFields.status = status
+    if (githubusername) profileFields.githubusername = githubusername
+
+    console.log(profileFields);
+    if (skills) {
+      profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+
+    console.log(profileFields.skills)
+
+    // Build Social Object  
+    profileFields.social = {}
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        // Update 
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile)
+      }
+      profile = new Profile(profileFields);
+      await Profile.save()
+      res.json(profile)
+
+    } catch (err) {
+
+    }
+
+
+
   }
 );
 
